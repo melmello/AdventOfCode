e27831cb1ba92d2fef97e325490bd0a5ea6f979f
Giulio Melloni <g.melloni@switchup.it>
2022-12-12 23:25:37 +0100

    feat(aoc): added solution for day 2 part 2

---
+++ 2022/12-02_2/main.jl
@@ -0,0 +1,30 @@
+#=
+main:
+- Julia version: 1.8.2
+- Author: melmello
+- Date: 2022-12-11
+=#
+elves = []
+open("input.txt") do file
+    total_score = 0
+    dict_play = Dict("A" => 1, "B" => 2, "C" => 3)
+    dict_result = Dict("X" => 0, "Y" => 3, "Z" => 6)
+    dict_win = Dict("A" => "C", "B" => "A", "C" => "B")
+    dict_lose = Dict("A" => "B", "B" => "C", "C" => "A")
+    for ln in eachline(file)
+        play = split(ln, " ")
+        if play[2] == "Y"
+            total_score += 3
+            total_score += dict_play[play[1]]
+        end
+        if play[2] == "X"
+            total_score += 0
+            total_score += dict_play[dict_win[play[1]]]
+        end
+        if play[2] == "Z"
+            total_score += 6
+            total_score += dict_play[dict_lose[play[1]]]
+        end
+    end
+    println(total_score)
+end
